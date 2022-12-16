c2e558dc5635e4a0c79c96056099a0faa5166fa6
Danil-891 <120020208+Danil-891@users.noreply.github.com>
2022-12-17 01:52:05 +0400

    Add files via upload

---
+++ ЗАДАНИЕ 11.pas
@@ -0,0 +1,12 @@
+﻿var s:string;
+begin
+  writeln('введите строку больше или меньше 10 ');
+  readln(s);
+  if length(s) > 10 then 
+    s:= copy(s,1,6);
+  writeln('Вывод если строка меньше 10 символов: ',s);
+  else
+  if length(s)<12 then 
+      s:= s+'o';
+  writeln('Вывод если строка больше 10 символов: ',s);
+end.
