af7b7dbbf67a71f2a84ae4759479945bda999456
Danil-891 <120020208+Danil-891@users.noreply.github.com>
2022-12-17 12:56:15 +0300

    Add files via upload

---
+++ 1 ЗАДАНИЕ.pas
@@ -0,0 +1,20 @@
+﻿var
+    s, s_old, s_new: string;
+    i, l_old: byte;
+begin
+    write('Введите строку: ');
+    readln(s);
+   s_old:=('Nikolay');
+   l_old := length(s_old);
+   s_new:=('Oleg');
+    i := 1;
+    while i <> 0 do begin
+        i := pos(s_old, s);
+        if i <> 0 then begin
+            delete(s, i, l_old);
+            insert(s_new, s, i);
+        end;
+    end;
+    writeln(s);
+end.
+
---
+++ 2 ЗАДАНИЕ.pas
@@ -0,0 +1,17 @@
+﻿var s:string;
+    n:integer;
+begin
+  writeln('введите что нибудь');
+  readln(s);
+  n:=length(s) * 3;
+  writeln(s,', ',s,', ',s);
+  writeln('количество символов =', n);
+end.
+
+
+
+
+
+
+
+    
