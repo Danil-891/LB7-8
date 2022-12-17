1fbf3183009fa1eb80108081aad215fde5b729a0
Danil-891 <120020208+Danil-891@users.noreply.github.com>
2022-12-17 12:57:03 +0300

    Add files via upload

---
+++ 4 ЗАДАНИЕ.pas
@@ -0,0 +1,10 @@
+﻿var s,n,l:string;
+begin
+  writeln('введите строку');
+  readln(s);
+  if length(s) >= 6 then
+  writeln('первые 3 символа = ',s[1],s[2],s[3], ' последние 3 символа = ',s[length(s) - 2],s[length(s) - 1], s[length(s)])
+  else
+  writeln(s[1] * length(s)); 
+end.
+
---
+++ 8 ЗАДАНИЕ.pas
@@ -0,0 +1,10 @@
+﻿var s: string;
+begin
+  writeln('Введите предложение, содержащее буквы x и w:');
+  readln(s);
+  if pos('w', s) < pos('x', s)
+    then write('x  встречается раньше');
+    if pos('x', s) < pos('w', s)
+    then write('w  встречается раньше')
+    else write('нету');
+end.
